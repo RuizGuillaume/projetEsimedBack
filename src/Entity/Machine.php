cbe0c2e86d22f3a24d5c395c3a99589f26457549
RuizGuillaume <63467093+RuizGuillaume@users.noreply.github.com>
2021-06-28 13:54:56 +0200

    Ajout Api Fournisseur/Machine/TypePiece/Utilisateur

--- src/Entity/Machine.php
+++ src/Entity/Machine.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\MachineRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=MachineRepository::class)
+ * @ApiResource()
  */
 class Machine
 {
--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\UtilisateurRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=UtilisateurRepository::class)
+ * @ApiResource()
  */
 class Utilisateur
 {
